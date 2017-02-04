70ad6d19e5ea436b4444e24901fb72d57b76bc40
Alexey Kurgan <ca@alljoint.ru>
2017-02-04 21:49:22 +0300

    Удалены устаревшие файлы

--- liferay/jetty8/shutdown.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 1251
-set JAVA_HOME=C:\Java\jdk1.8.0_40
-set PATH=%JAVA_HOME%\bin;%PATH%
-java -DSTOP.PORT=7171 -DSTOP.KEY=923hgkjdhf7833 -Xmx32m -jar start.jar --stop
--- liferay/jetty8/startup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 1251
-set JAVA_HOME=C:\Java\jdk1.8.0_40
-set PATH=%JAVA_HOME%\bin;%PATH%
-java -jar start.jar
